d77adf4d1e580fafd9d4c45c6cf46c5165bc2187
timothyoesch <54284649+timothyoesch@users.noreply.github.com>
2021-05-19 21:57:50 +0200

    Scroll Fix on mobile

--- wp-content/themes/unterschreib-jetzt/js/pages/frontpage.js
+++ wp-content/themes/unterschreib-jetzt/js/pages/frontpage.js
@@ -1,7 +1,9 @@
 jQuery("#mobile-fab").click(function(){
     jQuery("aside").addClass("form");
+    jQuery("html").addClass("noscroll");
 })
 
 jQuery(".close-icon").click(function(){
     jQuery("aside").removeClass("form");
+    jQuery("html").removeClass("noscroll");
 })
